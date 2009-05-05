d0d39cdd9d407cf214b142e6a68a4423abbc7b1e
cdmckay <me@cdmckay.org>
2009-05-05 19:23:53 +0000

    Added tests for set-css.

--- jss/tests/test-command.js
+++ jss/tests/test-command.js
@@ -67,11 +67,11 @@ test("remove-attr", function()
 	var sheet = { "#test1": {} };	
 	$.jss.declare(sheet);
 	
-	// Make sure the attribute is set to test to start.
+	// Grab the two test elements.
 	var $test1 = $('#test1');
 	var $test2 = $('#test2');
 	
-	// Test removing under ideal circumstances.
+	// Test remove-attr under ideal circumstances.
 	sheet["#test1"]["click"] = "remove-attr test";
 	$.jss.load();
 	$test1.attr("test", "test");
@@ -89,11 +89,53 @@ test("remove-attr", function()
 	equals($test1.attr("bean"), "boon", "Make sure attribute with the same name as the extra argument was untouched");
 	$test1.unbind();
 	
-	// Test removing under using a selector.
+	// Test remove-attr under using a selector.
 	sheet["#test1"]["click"] = "remove-attr (#test2) test";
 	$.jss.load();
 	$test2.attr("test", "test");
 	$test1.click();
 	equals($test2.attr("test"), undefined, "Use selector, make sure the attribute was removed");
 	$test1.unbind();
+});
+
+test ("set-css", function()
+{
+	// Clear all declared sheets.
+	$.jss.clear();
+	
+	// Grab the two test elements.
+	var $test1 = $('#test1');
+	var $test2 = $('#test2');
+	
+	// Declare an empty sheet.
+	var sheet = { "#test1": {} };	
+	$.jss.declare(sheet);	
+	
+	// Test set-css under ideal circumstances.	
+	sheet["#test1"]["click"] = "set-css font-family Trebuchet";
+	$.jss.load();	
+	$test1.css("font-family", "Not Trebuchet");
+	$test1.click();
+	equals($test1.css("font-family"), "Trebuchet", "Ideal circumstances, make sure the font-family changed");
+	
+	// Test set-css with two word font.
+	sheet["#test1"]["click"] = "set-css font-family Trebuchet MS";
+	$.jss.load();	
+	$test1.css("font-family", "Not Trebuchet MS");
+	$test1.click();
+	equals($test1.css("font-family"), "Trebuchet MS", "Two word font, make sure the font-family changed");
+	
+	// Test set-css under ideal circumstances with a selector.
+	sheet["#test1"]["click"] = "set-css (#test2) font-family Trebuchet";
+	$.jss.load();	
+	$test2.css("font-family", "Not Trebuchet");
+	$test1.click();
+	equals($test2.css("font-family"), "Trebuchet", "Ideal circumstances with selector, make sure the font-family changed");
+	
+	// Test set-css with two word font and selector.
+	sheet["#test1"]["click"] = "set-css (#test2) font-family Trebuchet MS";
+	$.jss.load();	
+	$test2.css("font-family", "Not Trebuchet MS");
+	$test1.click();
+	equals($test2.css("font-family"), "Trebuchet MS", "Two word font with selector, make sure the font-family changed");
 });
